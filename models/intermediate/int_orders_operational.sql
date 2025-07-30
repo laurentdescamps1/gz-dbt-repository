da08a310d0882bdcf091be47810d89a0638466fd
LAURENT DESCAMPS <laurent.descamps1@gmail.com>
2025-07-30 09:14:15 +0000

    commit table materialization

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,8 +1,15 @@
-SELECT 
-    o.orders_id
-    , o.date_date
-    , o.margin + s.shipping_fee - s.logcost - s.ship_cost AS operational_margin
-
-FROM {{ ref('int_orders_margin') }} AS o
-INNER JOIN {{ ref('stg_raw__ship') }} AS s
-ON s.orders_id = o.orders_id
+ SELECT
+     o.orders_id
+     ,o.date_date
+     ,ROUND(o.margin + s.shipping_fee - (s.logcost + s.ship_cost),2) AS operational_margin
+     ,o.quantity
+     ,o.revenue
+     ,o.purchase_cost
+     ,o.margin
+     ,s.shipping_fee
+     ,s.logcost AS log_cost
+     ,s.ship_cost
+ FROM {{ref("int_orders_margin")}} o
+ LEFT JOIN {{ref("stg_raw__ship")}} s
+     USING(orders_id)
+ ORDER BY orders_id desc
